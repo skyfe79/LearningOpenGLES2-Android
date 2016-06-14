9a3d8cb21f52e8bfa0e3fc3b8d6a664f3ca65ed7
burt <burt@users-iMac.local>
2016-06-15 01:28:28 +0900

    implement ShaderProgram

---
+++ 02.Star/app/src/main/java/kr/pe/burt/android/star/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.pe.burt.android.star;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
