d0da7244a0669e9907713a85e9da72e32a89310a
burt <burt@users-iMac.local>
2016-06-22 02:17:47 +0900

    android emulator does not support opengl es 3.0 :-(

---
+++ 04.VertexArrayObject/app/src/main/java/kr/pe/burt/android/vertexarrayobject/MainActivity.java
@@ -0,0 +1,41 @@
+package kr.pe.burt.android.vertexarrayobject;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private OGLView oglView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        oglView = (OGLView) findViewById(R.id.oglView);
+
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        String versionText = "Device Supported OpenGL ES Version = " + configurationInfo.getGlEsVersion();
+        Toast.makeText(this, versionText, Toast.LENGTH_LONG).show();
+        Log.d("TAGTAG", versionText);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        oglView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        oglView.onResume();
+    }
+}
