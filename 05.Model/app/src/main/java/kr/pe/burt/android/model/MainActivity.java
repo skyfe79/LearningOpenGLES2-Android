e5bc7bd88aa62003b9687100d8b88279a8440a16
burt <burt@users-iMac.local>
2016-06-22 23:31:20 +0900

    make Model abstract class

--- 05.Model/app/src/main/java/kr/pe/burt/android/model/MainActivity.java
+++ 05.Model/app/src/main/java/kr/pe/burt/android/model/MainActivity.java
@@ -1,4 +1,4 @@
-package kr.pe.burt.android.coloredindexsquare;
+package kr.pe.burt.android.model;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ 05.Model/app/src/main/java/kr/pe/burt/android/model/OGLRenderer.java
@@ -0,0 +1,48 @@
+package kr.pe.burt.android.model;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import kr.pe.burt.android.model.glkit.ShaderProgram;
+import kr.pe.burt.android.model.glkit.ShaderUtils;
+
+/**
+ * Created by burt on 2016. 6. 15..
+ */
+public class OGLRenderer implements GLSurfaceView.Renderer {
+
+    private Context context;
+    private Square square;
+
+    public OGLRenderer(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+        ShaderProgram shader = new ShaderProgram(
+                ShaderUtils.readShaderFileFromRawResource(context, R.raw.simple_vertex_shader),
+                ShaderUtils.readShaderFileFromRawResource(context, R.raw.simple_fragment_shader)
+        );
+        square = new Square(shader);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int w, int h) {
+        GLES20.glViewport(0, 0, w, h);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        square.draw();
+    }
+
+}
