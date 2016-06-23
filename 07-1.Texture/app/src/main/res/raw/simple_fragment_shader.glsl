99ef85596fbebad8017568a37c30e237fdd1a2b6
burt <burt@users-iMac.local>
2016-06-24 00:49:55 +0900

    render texture on the cube

---
+++ 07-1.Texture/app/src/main/res/raw/simple_fragment_shader.glsl
@@ -0,0 +1,10 @@
+precision mediump float;
+
+uniform sampler2D u_Texture;
+
+varying lowp vec4 frag_Color;
+varying lowp vec2 frag_TexCoord;
+
+void main() {
+    gl_FragColor = frag_Color * texture2D(u_Texture, frag_TexCoord);
+}
