067837760f65c179ec13994058179b4be1685a02
burt <burt@users-iMac.local>
2016-06-25 22:48:25 +0900

    preparing masking

---
+++ 07-3.Masking/app/src/main/java/kr/pe/burt/android/masking/MaskedSquare.java
@@ -0,0 +1,36 @@
+package kr.pe.burt.android.masking;
+
+import kr.pe.burt.android.masking.glkit.ShaderProgram;
+
+/**
+ * Created by burt on 2016. 6. 25..
+ */
+public class MaskedSquare extends Model {
+
+    static final float vertices[] = {
+             1.0f, -1.0f, 0f,   1f, 0f,
+             1.0f,  1.0f, 0f,   1f, 1f,
+            -1.0f,  1.0f, 0f,   0f, 1f,
+            -1.0f, -1.0f, 0f,   0f, 0f
+
+    };
+
+    static final short indices[] = {
+            0, 1, 2,
+            2, 3, 0
+    };
+
+
+
+    public MaskedSquare(ShaderProgram shader) {
+        super("MaskedSquare", shader, new float[] {
+                1.0f, -1.0f, 0f,   1f, 0f,
+                1.0f,  1.0f, 0f,   1f, 1f,
+                -1.0f,  1.0f, 0f,   0f, 1f,
+                -1.0f, -1.0f, 0f,   0f, 0f
+        }, new short[] {
+                0, 1, 2,
+                2, 3, 0
+        });
+    }
+}
